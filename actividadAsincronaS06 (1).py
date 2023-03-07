1993bec96cf619fa91e5ad14a7795545689f33a8
rlcosta32 <126819207+rlcosta32@users.noreply.github.com>
2023-03-06 21:51:37 -0600

    Add files via upload

---
+++ actividadAsincronaS06 (1).py
@@ -0,0 +1,41 @@
+# Definición de las variables
+valor1 = 10
+valor2 = 5
+valor3 = 7
+valor4 = 4
+
+# Suma
+print("Realizando suma...")
+result_suma = valor1 + valor2 + valor3
+print("El resultado de la suma es:", result_suma)
+
+# Resta
+print("Realizando resta...")
+result_resta = valor4 - valor2 - valor1 - valor3
+print("El resultado de la resta es:", result_resta)
+
+# Multiplicación
+print("Realizando multiplicación...")
+result_multiplicacion = valor1 * valor2
+result_multiplicacion = result_multiplicacion * (valor2 + valor2)
+print("El resultado de la multiplicación es:", result_multiplicacion)
+
+# Exponente
+print("Realizando exponenciacion...")
+result_exponente = valor1 ** valor2
+print("El resultado del exponente es:", result_exponente)
+
+# Módulo
+print("Realizando módulo...")
+result_mod = valor1 % valor4
+print("El resultado del módulo es:", result_mod)
+
+# División
+print("Realizando división...")
+result_division = result_exponente / result_mod
+print("El resultado de la división es:", result_division)
+
+# División entera
+print("Realizando división entera...")
+result_division_entera = result_exponente // result_mod
+print("El resultado de la división entera es:", result_division_entera)
